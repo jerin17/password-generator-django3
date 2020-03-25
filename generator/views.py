a359ac951ac4fa077e399ac3aaf1637abf9b0709
Jerin <jerinthomas17@gamil.com>
2020-03-26 02:22:08 +0530

    generate password and polish

--- generator/views.py
+++ generator/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+import random
 
 def home(request):
 	return render(request, 'home.html')
 
 def password(request):
-	password = 123
-	return render(request, 'password.html',{'password':password})
+	password = ""
+	char = list('qwertyuiopasdfghjklzxcvbnm')
+	len = int(request.GET.get('length',12))
+
+	if request.GET.get('uppercase'):
+		char.extend(list('QWERTYUIOPASDFGHJKLZXCVBNM'))
+	if request.GET.get('numbers'):
+		char.extend(list('1234567890'))
+	if request.GET.get('special'):
+		char.extend(list('!@#$%^&*()_+?>:~'))
+
+	for x in range(len):
+		password += random.choice(char)
+
+	return render(request, 'password.html',{'password':password, 'len':len})
